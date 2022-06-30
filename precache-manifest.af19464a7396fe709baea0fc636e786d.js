00135e3db3fea8e7e236bb918ec4e499b219b51b
Cristian <solgar@gmail.com>
2022-06-29 22:26:46 -0600

    Updates

---
+++ precache-manifest.af19464a7396fe709baea0fc636e786d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ebaf1aa95251a2044bd",
+    "url": "/demoReact.github.io/static/css/main.7902f80d.chunk.css"
+  },
+  {
+    "revision": "1ebaf1aa95251a2044bd",
+    "url": "/demoReact.github.io/static/js/main.1ebaf1aa.chunk.js"
+  },
+  {
+    "revision": "90320a4596191f6bbc0d",
+    "url": "/demoReact.github.io/static/js/1.90320a45.chunk.js"
+  },
+  {
+    "revision": "c6ab35c6ad121c46b727",
+    "url": "/demoReact.github.io/static/js/2.c6ab35c6.chunk.js"
+  },
+  {
+    "revision": "db37fec54062834b59a5",
+    "url": "/demoReact.github.io/static/js/runtime~main.db37fec5.js"
+  },
+  {
+    "revision": "a239a33119107919788a0337a8a7cfd3",
+    "url": "/demoReact.github.io/index.html"
+  }
+];
